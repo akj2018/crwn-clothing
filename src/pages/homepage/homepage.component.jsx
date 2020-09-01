dbe013b81dd87ca8e53cc9b45246c22315a153f7
Akshay Jain <jainakshay1783@gmail.com>
2020-09-01 07:53:52 +0530

    Added Routing using react-router-dom on Homepage and initialized ShopPage

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,6 +4,7 @@ import "./homepage.styles.scss";
 import Directory from "../../components/directory/directory.component";
 
 const HomePage = (props) => {
+  console.log(props.match);
   return (
     <div className="homepage">
       <Directory />
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import SHOP_DATA from "./shop.data";
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collextions: SHOP_DATA,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>SHOP PAGE</h1>
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
