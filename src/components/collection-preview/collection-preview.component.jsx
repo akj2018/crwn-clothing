73f06e4fb0331efa331ab4e8c8e871f41cc33b00
Akshay Jain <jainakshay1783@gmail.com>
2020-09-01 08:53:28 +0530

    App Updated with ShopPage, CollectionPreview Cpmponent and CollectionItem Component

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./collection-preview.styles.scss";
+import "../collection-item/collection-item.component";
+import CollectionItem from "../collection-item/collection-item.component";
+
+const CollectionPreview = ({ title, items }) => {
+  return (
+    <div className="collection-preview">
+      <h2 className="title">{title.toUpperCase()}</h2>
+      <div className="preview">
+        {items
+          .filter((item, index) => index < 4)
+          .map(({ id, ...otherItemProps }) => (
+            <CollectionItem key={id} {...otherItemProps} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default CollectionPreview;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 
 import SHOP_DATA from "./shop.data";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
 class ShopPage extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      collextions: SHOP_DATA,
+      collections: SHOP_DATA,
     };
   }
 
   render() {
     return (
-      <div>
-        <h1>SHOP PAGE</h1>
+      <div className="shop">
+        {this.state.collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
       </div>
     );
   }
